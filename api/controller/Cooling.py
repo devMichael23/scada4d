15ffd8739ccb63df7c9e5cf9cd55d241ba052f04
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2023-03-09 13:00:29 +0300

    add cooling controller

--- api/controller/Cooling.py
+++ api/controller/Cooling.py
@@ -1,3 +1,21 @@
-from imports.General import *
+from core.Vars import *
+from core.SCADAVars import *
 
 
+async def __controllerCoolingChange(refrigerantActive: SCADAVar,
+                                    isCoolingOnServer: bool,
+                                    value):
+    if isCoolingOnServer:
+        await refrigerantActive.setValue(value)
+
+
+async def controllerCoolingEnable(scadaVars: scadaVars_t):
+    await __controllerCoolingChange(scadaVars[idRefrigerantActive],
+                                    await scadaVars[idIsOnCoolingWithServer].getValue(),
+                                    true_t)
+
+
+async def controllerCoolingDisable(scadaVars: scadaVars_t):
+    await __controllerCoolingChange(scadaVars[idRefrigerantActive],
+                                    await scadaVars[idIsOnCoolingWithServer].getValue(),
+                                    false_t)
