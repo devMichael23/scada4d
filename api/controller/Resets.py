7ea249152d379e4f10c9496a4f4e98c33bdd89e7
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2023-03-16 23:44:59 +0300

    fix

--- api/controller/Resets.py
+++ api/controller/Resets.py
@@ -10,6 +10,11 @@ async def controllerResetIsServerHarmed(var: SCADAVar):
     await var.setValue(valIsServerHarmed)
 
 
+async def controllerResetRefrigerantActive(var: SCADAVar):
+    await var.setValue(valRefrigerantActive)
+
+
 async def controllerResetAll(scadaVars: scadaVars_t):
     await controllerResetCurrentTemp(scadaVars[idCurrentTemp])
     await controllerResetIsServerHarmed(scadaVars[idIsServerHarmed])
+    await controllerResetRefrigerantActive(scadaVars[idRefrigerantActive])
